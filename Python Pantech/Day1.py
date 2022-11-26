14c25994de8d80114af69cfaaa606fd52364f9a0
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-26 19:52:41 +0530

    Introduction to Python

---
+++ Python Pantech/Day1.py
@@ -0,0 +1,39 @@
+'''
+30 Days Advanced Python Programming  Masterclass 
+Day 1 - Introduction to Python
+Day 2 - Python - Tools| Syntaxes and Data Structures
+Day 3 - Python - Dictionaries, Lists, Tuples, Functions
+Day 4 - Python - Classes and Objects , Reg Expressions
+Day 5 - Python - Iterators , Modules
+Day 6 - Python - Packages, Generators
+Day 7 - Python - List Comprehensions, Serialization
+Day 8 - Python - Partial Functions , Closures
+Day 9 - Python - Decorators, Python - Examples
+Day 10 - Python - Networking , Multithreading
+Day 11 - Pandas library - Introduction
+Day 12 - Pandas library - Data Structures
+Day 13 - Numpy library - Introduction
+Day 14 - Numpy library - Array Manipulations , Mathematical Functions
+Day 15 - Matplotlib , Histogram using Matplotlib | I/O with Numpy
+Day 16 - Matplotlib library - Pyplot API , Types of plots
+Day 17 - Seaborn library
+Day 18 - SKLearn library
+Day 19 - Keras Library
+Day 20 - Automation In Python
+Day 21 - Weight Converter GUI with Python , Create a ChessBoard Using Python
+Day 22 - Card Game Using Python,Python Clock App With Kiwi
+Day 23 - Turtle Graphics With Python , Monty Hall Simulator Using Python
+Day 24 - Chatbot creation in Python    
+Day 25 - Electricity Price Prediction Using ML          
+Day 26 - Customer Segmentation Using ML
+Day 27 - House Price Prediction
+Day 28- Intrusion Detection using ML
+Day 29 - Android Malware Detection
+Day 30- Churn Prediction
+'''
+print("Welcome to advanced python programming")
+print(2*3)
+a=10
+b=20
+c=a+b
+print(c)
