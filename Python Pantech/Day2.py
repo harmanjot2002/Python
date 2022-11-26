81a4526c666f38b97e6cc6fb27cec7b1f67d841c
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-26 20:34:27 +0530

    Tools,Syntax and Data Structures

---
+++ Python Pantech/Day2.py
@@ -0,0 +1,47 @@
+#Datatypes
+a=10
+b=20.2
+c="pillow"
+d='L'
+print(b)
+print(a)
+
+#Conversion Function
+print(int(4.5))
+
+#Variables
+website="apple.com"
+print(website)
+
+a,b,c=5,3.2,"Hello"
+print(a)
+print(b)
+print(c)
+
+x=y=z="Same"
+print(y)
+
+a=20
+b=30
+c=a+b
+print(c)
+
+print(type(3.3))
+print(type(2))
+
+#Format specifiers
+"""
+    %d
+    %F
+    %s
+    %c
+    %ld
+"""
+
+#relational operators
+print(6==6)
+print(8>6)
+print(8==9)
+print(4==4 and 3<7)
+print(5==5 or 1>10)
+
