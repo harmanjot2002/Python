4acb1abe4448798b1d2813f77e24f09f1b23f3ba
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-27 18:32:40 +0530

    Decorators and Python

---
+++ Python Pantech/Day9.py
@@ -0,0 +1,4 @@
+#Decorators are a powerful and useful tool in Python since it allows programmers to modify the behaviour of a function or class.
+#Decorators allow us to wrap another function in order to extend the behaviour of the wrapped function,without permanently modifying it.
+
+
