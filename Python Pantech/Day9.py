4b7f9e186bcd36504ad9aaf88cfe18d6efc4d076
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-27 18:41:37 +0530

    Decorators and Python

--- Python Pantech/Day9.py
+++ Python Pantech/Day9.py
@@ -16,3 +16,12 @@ def greet(func):
     print(greeting)
 greet(shout)
 greet(whisper)
+
+#Returning function from another function
+def create_adder(x):
+    def adder(y):
+        return x+y
+    return adder
+
+add_15=create_adder(15)
+print(add_15(10))
