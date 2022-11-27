e54766039e5f1a223d3f5c50bcc3992d8829f37d
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-27 19:31:33 +0530

    Pandas Library-Introduction

---
+++ Python Pantech/Day11.py
@@ -0,0 +1,18 @@
+#Pandas is a open-source python library.
+#It provides highly efficient data structures and data analysis tools for python programming languages.
+#Python with pandas is used in a variety of domains like academics,finance,economics,statics.
+#Pandas use most of the functionalities of numpy.
+#It deals with the following three data structures:
+    #1.Series
+    #2.Data frame
+    #3.Panel
+#pip install pandas
+#Once Anaconda is installed,Pandas will be installed with it.
+#Anaconda is a open-source python distribution for scipy.
+#It is available for linux and mac.
+#Higher dimensional data structure is a container of lower dimensional data structure.
+#Data-Frame is a container of series and panel is a container of data frame.
+#Series-It is a one-dimensional collection of similar elements.Series is nothing,but a collection of integers.
+    #1.Collection of cimilar elements.
+    #2.Size cannot be changed(immutable)
+    #3.Values of data can be changed(mutable)
