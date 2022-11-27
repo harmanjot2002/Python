f2b5ecb12f88a1c8c48511ada4a4af434197f8e0
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-27 18:29:05 +0530

    Partial Functions and Closures

--- Python Pantech/Day8.py
+++ Python Pantech/Day8.py
@@ -10,8 +10,7 @@ def f(a,b,c,x):
 g=partial(f,3,1,4)
 print(g(5))
 
-#In this,we have pre-filled our function with some 
-#constant values of a,b,c and g() takes single arg(x)
+#In this,we have pre-filled our function with some constant values of a,b,c and g() takes single arg(x)
 
 from functools import *
 
@@ -24,18 +23,20 @@ print(add_part(3))
 #Nested Functions
 def outerFunction(text):
     text=text
-def innerFunction():
-    print(text)
+    def innerFunction():
+        print(text)
     innerFunction()
 if __name__=='__main__':
     outerFunction('Hey!')
 
 #Closures
 #A closure allows the function to access those captures variables through the closure's copies of their values of references,even when function is invoked outside their scope.
+#Closures help to invoke functions outside their scope.
+#
 
 def outerFunction(text):
     text=text
-    def innerFuntion():
+    def innerFunction():
         print(text)
     return innerFunction
 if __name__=="__main__":
