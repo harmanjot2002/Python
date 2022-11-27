1690801c72faf03b48b579b1a880a74665a36ff1
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-27 17:56:09 +0530

    Iterators and Modules

--- Python Pantech/Day5.py
+++ Python Pantech/Day5.py
@@ -1,3 +1,7 @@
+# Iterator in python is an object that is used to iterate over iterable objects like lists,tuples,dictionaries and sets.
+#Iterator Object is initialized using iter() method.
+# It uses the next() method for iteration. 
+
 iterable_value="Pantech"
 iterable_obj=iter(iterable_value)
 
@@ -37,6 +41,16 @@ for i in Test(15):
 for i in Test(5):
     print (i)
 
+d=dict()
+d['xyz']=123
+d['abc']=345
+for i in d:
+    print("%s %d" %(i,d[i]))
+
+
+#A python module is a file containing Python definitions and statements.
+#A module caan define functions,classes and variables.
+#A module can also include runnable code.
 from math import sqrt,factorial
 print(sqrt(25))
 print(factorial(5)) 
