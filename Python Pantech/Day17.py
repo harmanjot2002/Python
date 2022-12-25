c601466b85435232d0650e45af23c33bcdcbb2d7
harmanjot2002 <harmanjotkainth@gmail.com>
2022-12-25 12:47:11 +0530

    Seaborn library

---
+++ Python Pantech/Day17.py
@@ -0,0 +1,13 @@
+"""
+Seaborn:
+    -It is a Python Data Visualization library based on Matplotlib.
+    -It provides high-level interface for drawing attractive and informative statistical graphics.
+    -It provides default styles and color palettes to make statistical plots more attractive.
+    -Seaborn aims to make visualization the central part of exploring and understanding data.
+    -It provides dataset-oriented APIs,so that we can switch between different varieties of representations of same variable for better undestanding of the dataset.
+    -Types of plots in Seaborn:
+        -Relational Plots:This plot is used to understand the relation between two variables.
+        -Categorical Plots:This plot deals with categorical variables and how they can be visualized.
+        -Distribution Plots:This plot is used for examining univariate and bivariate distributions.
+        -Regression Plots:They are intended to add a visual guide that helps to emphasize patterns in a dataset during EDA.
+"""
